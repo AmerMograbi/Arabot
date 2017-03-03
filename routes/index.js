5778275f1cc1711d9762880288763ff5f60832ed
Amer Mograbi <amoor.mograbi@gmail.com>
2017-03-03 18:58:09 +0200

    changed to users_table

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var pg = require('pg');
 
 router.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM users_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
