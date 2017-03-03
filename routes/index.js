096c159e4d70ac697f5150db32a2907ed23c1356
Amer Mograbi <amoor.mograbi@gmail.com>
2017-03-03 20:28:39 +0200

    removed render from db as well

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('db', {results: result.rows} ); }
+       { response.send(result.rows); }
     });
   });
 });
