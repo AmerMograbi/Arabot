e90f26124ba8adac44b3b443621008d0d6ce9491
Amer Mograbi <amoor.mograbi@gmail.com>
2017-03-03 18:44:01 +0200

    added psql db

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('db', {results: result.rows} ); }
+    });
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
