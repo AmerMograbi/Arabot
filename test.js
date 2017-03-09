9d165a7c01d178ecd663ad0ba416a6d6766ecd59
Amer Mograbi <amoor.mograbi@gmail.com>
2017-03-09 17:53:52 +0200

    checked for property quick_reply

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ const fbMessenger = require('./lib/fb-messenger.js');
 
 
 
-const event = {
-	message:{
+const mockQuickReplyMessage = {
+	message: {
 		quick_reply: {
 			payload: "showTypes->foreign series"
 		},
@@ -23,11 +23,21 @@ const event = {
 
 };
 
+const mockTextMessage = {
+	message: {
+		text: "yo foo"
+	},
+	sender: {
+		id: "123"
+	},
+	recipient: {
+		id: "456"
+	},
+	timestamp: "789"
+
+};
+
 //var msg = messageBuilder.getQuickReplyResponse("showTypes-foreign movie", 123);
-var msg = messageBuilder.getQuickReplyResponse("showTypes->foreign movie", 123);
-//fbMessenger.receivedMessage(event);
+//var msg = messageBuilder.getQuickReplyResponse("showTypes->foreign movie", 123);
+fbMessenger.receivedMessage(mockQuickReplyMessage);
 //var msg = messageBuilder.getGettingStartedResponse("123");
-
-
-
-
