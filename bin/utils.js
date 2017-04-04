426266708c335602e1077f6638216b623997fcc9
Amer Mograbi <amoor.mograbi@gmail.com>
2017-04-04 12:31:47 +0300

    removed short checkPeriod from cache

--- bin/utils.js
+++ bin/utils.js
@@ -23,7 +23,7 @@ function run() {
 			//printUsers();		
 			//findUser("123");
 			//printAllInCollection(turkishSeries);
-			//database.dropCollection("users").then(res => console.log("collection dropped. res:" + res));
+			database.dropCollection("users").then(res => console.log("collection dropped. res:" + res));
 		})
 		.catch((err) => console.log(err));
 }
